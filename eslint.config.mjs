bf30654d60d1cda71d8923a60a1a14020b2c5ea5
Alex Stoev <alex.stoev@gmail.com>
2025-10-31 18:24:23 +0200

    dev env setup

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,11 +1,40 @@
-import { defineConfig, globalIgnores } from "eslint/config";
 import nextVitals from "eslint-config-next/core-web-vitals";
 import nextTs from "eslint-config-next/typescript";
+import prettier from "eslint-config-prettier/flat";
+import checkFile from "eslint-plugin-check-file";
+import { defineConfig, globalIgnores } from "eslint/config";
 
 const eslintConfig = defineConfig([
   ...nextVitals,
   ...nextTs,
+  prettier,
+
   // Override default ignores of eslint-config-next.
+  {
+    plugins: {
+      "check-file": checkFile,
+    },
+    rules: {
+      "prefer-arrow-callback": "error",
+      "check-file/filename-naming-convention": [
+        "error",
+        {
+          "**/*.{ts,tsx}": "KEBAB_CASE",
+        },
+        {
+          // ignore the middle extensions of the filename to support filename like bable.config.js or smoke.spec.ts
+          ignoreMiddleExtensions: true,
+        },
+      ],
+      "check-file/folder-naming-convention": [
+        "error",
+        {
+          // all folders within src (except __tests__)should be named in kebab-case
+          "src/**/!(__tests__)": "KEBAB_CASE",
+        },
+      ],
+    },
+  },
   globalIgnores([
     // Default ignores of eslint-config-next:
     ".next/**",
